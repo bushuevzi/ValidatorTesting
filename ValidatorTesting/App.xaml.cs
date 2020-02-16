dbde016c0d0da740290372ded3999ae33255a17e
Bushuev Zakhar <bushuevzi@mail.ru>
2020-02-16 14:38:50 +0300

    Create view

---
+++ ValidatorTesting/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ValidatorTesting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ ValidatorTesting/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ValidatorTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ValidateButton(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
